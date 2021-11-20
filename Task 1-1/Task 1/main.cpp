5ccc2a5cfa51ca0e7af54779d1b8a46adca6f855
kisany1975 <kisany1975@gmail.com>
2021-11-20 23:44:47 +0300

    Переделанная задача 1-1

--- Task 1-1/Task 1/main.cpp
+++ Task 1-1/Task 1/main.cpp
@@ -3,22 +3,47 @@
 #include <iostream>
 using namespace std;
 
-
 /**
-*\brief Поиск значения a и b
+* brief Функция для вычисления значения A
 * param x параметр x
 * param y параметр y
 * param z параметр z
-* return значения a и b
+* return значение A
+*/
+double getA(const double x, const double y, const double z);
+
+/**
+* brief Функция для вычисления значения B
+* param x параметр x
+* param y параметр y
+* param z параметр z
+* return значение B
+*/
+double getB(const double x, const double y, const double z);
+
+
+/**
+* brief Точка входа в программу
+* return 0 в случае успеха
 */
 int main()
 {
     const double x = -2.9;
     const double y = 15.5;
     const double z = 1.5;
-    double a = sqrt(x * x + y) - y * y * sin(x + z / x) * sin(x + z / x) * sin(x + z / x);
-    double b = cos(x * x * x) * cos(x * x * x) - x / sqrt(z * z + y * y);
-    cout << a << endl;
-    cout << b;
+    double a = getA(x, y, z);
+    double b = getB(x, y, z);
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
     return 0;
+}
+
+double getA(const double x, const double y, const double z)
+{
+    return sqrt(pow(x, 2) + y) - pow(y, 2) * pow(sin(x + y / x), 3);
+}
+
+double getB(const double x, const double y, const double z)
+{
+    return cos(pow(x, 3)) * cos(pow(x, 3)) - x / sqrt(pow(x, 2) + pow(y, 2));
 }
